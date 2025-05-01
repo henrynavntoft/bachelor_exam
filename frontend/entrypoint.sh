cdd7fb2ec04e78e108df64c31044775f465d43c0
Henry Navntoft <henrylnavntoft@gmail.com>
2025-05-01 14:07:48 +0200

    "..."

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -4,7 +4,11 @@ echo "== Frontend running in $RTE mode =="
 
 if [ "$RTE" = "prod" ]; then
   echo "=== Production Mode ==="
-  npm run dev
+  if [ ! -d ".next" ]; then
+    echo "No .next folder found, running build..."
+    npm run build
+  fi
+  npm run start
 else
   echo "=== Development Mode ==="
   npm run dev
